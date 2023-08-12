a3b54b149c59761cf0a520c67821fe0e88c7620d
wygrajr <wycliffe.graftonjr@gmail.com>
2023-08-12 17:25:07 -0400

    Departments are now printing in console

---
+++ index.js
@@ -0,0 +1,35 @@
+// Import and require mysql2
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password here
+    password: 'root',
+    database: 'employees_db'
+  },
+  console.log(`Connected to the employees_db database.`)
+);
+
+async function getDepartments() {
+  const sql = `SELECT * FROM employees_db.departments;`
+
+  return new Promise((resolve, reject) => {
+    db.query(sql, (err, result) => {
+      if (err) {
+        console.error(err)
+        reject(err)
+      }
+      resolve(result)
+    })
+  })
+}
+
+getDepartments().then(result => {
+  console.log(result)
+})
+
+
