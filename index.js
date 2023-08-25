3bf59692012dad0a396266fcc9200c0fa4de3ba4
wygrajr <wycliffe.graftonjr@gmail.com>
2023-08-25 14:08:09 -0400

    inquirer was wrapped in async function. Break was added before each case. Update employee KV pair was fixed. SQL updated to take all inputed values

--- index.js
+++ index.js
@@ -75,7 +75,8 @@ async function addDepartment(departmentName) {
 }
 
 async function addRole(title,salary,departmentId) {
-  const sql = `INSERT INTO roles (title,salary,department_id) VALUES (?)`;
+  console.log(`${title} ${salary} ${departmentId}`)
+  const sql = `INSERT INTO roles (title,salary,department_id) VALUES (?,?,?)`;
   const params = [title,salary,departmentId];
 
   return new Promise((resolve, reject) => {
@@ -90,7 +91,7 @@ async function addRole(title,salary,departmentId) {
 }
 
 async function addEmployees(firstName,lastName,roleId,managerId) {
-  const sql = `INSERT INTO employees (first_name,last_name,role_id,manager_id) VALUES (?)`;
+  const sql = `INSERT INTO employees (first_name,last_name,role_id,manager_id) VALUES (?,?,?,?)`;
   const params = [firstName,lastName,roleId,managerId];
 
   return new Promise((resolve, reject) => {
@@ -106,7 +107,7 @@ async function addEmployees(firstName,lastName,roleId,managerId) {
 
 async function updateEmployee(employeeId, roleId) {
   const sql = `UPDATE employees_db.employees SET role_id = ? WHERE id = ?;`;
-  const params = [employeeId, roleId];
+  const params = [roleId, employeeId];
 
   return new Promise((resolve, reject) => {
     db.query(sql, params, (err, result) => {
@@ -119,101 +120,116 @@ async function updateEmployee(employeeId, roleId) {
   })
 }
 
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      message: 'Select an Option',
-      name: 'option',
-      choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
-    },
-  ])
-  .then(async (data) => {
-    switch (data.option) {
-      case "view all departments":
-          console.table(await getDepartments());
-      case "view all roles":
-          console.table(await getRoles());
-      case "view all employees":
-          console.table(await getEmployeess());
-      case "add a department":
-        inquirer
-        .prompt([
-          {
-            type: 'input',
-            message: 'Enter the name of department',
-            name: 'departmentName',
-          },
-        ])
-        .then(async (data) => {
-          await addDepartment(data.departmentName);
-        });
-      case "add a role":
-        inquirer
-        .prompt([
-          {
-            type: 'input',
-            message: 'Enter the title of role',
-            name: 'titleName',
-          },
-          {
-            type: 'input',
-            message: 'Enter the salary of role',
-            name: 'salary',
-          },
-          {
-            type: 'input',
-            message: 'Enter the department ID of role',
-            name: 'departmentId',
-          },
-        ])
-        .then(async (data) => {
-          await addRole(data.titleName,data.salary,data.departmentId);
-        });
-      case "add an employee":
-        inquirer
-        .prompt([
-          {
-            type: 'input',
-            message: 'Enter the first name',
-            name: 'firstName',
-          },
-          {
-            type: 'input',
-            message: 'Enter the last name',
-            name: 'lastName',
-          },
-          {
-            type: 'input',
-            message: 'Enter the role ID',
-            name: 'roleId',
-          },
-          {
-            type: 'input',
-            message: 'Enter the manger ID',
-            name: 'managerId',
-          },
-        ])
-        .then(async (data) => {
-          await addEmployees(data.firstName,data.lastName,data.roleId,data.managerId);
-        });
-      case "update an employee role":
-        inquirer
-        .prompt([
-          {
-            type: 'input',
-            message: 'Enter the employee ID',
-            name: 'employeeId',
-          },
-          {
-            type: 'input',
-            message: 'Enter the role ID',
-            name: 'roleId',
-          },
-        ])
-        .then(async (data) => {
-          await updateEmployee(data.employeeId,data.roleId);
-        });
-  }})
+async function runLoop() {
+  while (true) {
+    await inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: 'Select an Option',
+        name: 'option',
+        choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
+      },
+    ])
+    .then(async (data) => {
+      switch (data.option) {
+        case "view all departments":
+            console.table(await getDepartments());
+            break;
+        case "view all roles":
+            console.table(await getRoles());
+            break;
+        case "view all employees":
+            console.table(await getEmployeess());
+            break;
+        case "add a department":
+          await inquirer
+          .prompt([
+            {
+              type: 'input',
+              message: 'Enter the name of department',
+              name: 'departmentName',
+            },
+          ])
+          .then(async (data) => {
+            await addDepartment(data.departmentName);
+          });
+          break;
+        case "add a role":
+          await inquirer
+          .prompt([
+            {
+              type: 'input',
+              message: 'Enter the title of role',
+              name: 'titleName',
+            },
+            {
+              type: 'input',
+              message: 'Enter the salary of role',
+              name: 'salary',
+            },
+            {
+              type: 'input',
+              message: 'Enter the department ID of role',
+              name: 'departmentId',
+            },
+          ])
+          .then(async (data) => {
+            await addRole(data.titleName,data.salary,data.departmentId);
+          });
+          break;
+        case "add an employee":
+          await inquirer
+          .prompt([
+            {
+              type: 'input',
+              message: 'Enter the first name',
+              name: 'firstName',
+            },
+            {
+              type: 'input',
+              message: 'Enter the last name',
+              name: 'lastName',
+            },
+            {
+              type: 'input',
+              message: 'Enter the role ID',
+              name: 'roleId',
+            },
+            {
+              type: 'input',
+              message: 'Enter the manger ID',
+              name: 'managerId',
+            },
+          ])
+          .then(async (data) => {
+            await addEmployees(data.firstName,data.lastName,data.roleId,data.managerId);
+          });
+          break;
+        case "update an employee role":
+          await inquirer
+          .prompt([
+            {
+              type: 'input',
+              message: 'Enter the employee ID',
+              name: 'employeeId',
+            },
+            {
+              type: 'input',
+              message: 'Enter the role ID',
+              name: 'roleId',
+            },
+          ])
+          .then(async (data) => {
+            await updateEmployee(data.employeeId,data.roleId);
+          });
+          break;
+    }})
+  }
+}
+
+runLoop()
+
+
 
 
